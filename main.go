537e3ab81f0590092f6ee768c5a8c1f5869a76af
Kiran Malvi <kiran.malvi@mindinventory.com>
2019-04-22 16:16:21 +0530

    Initial Commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	u "Golang-HTMLTOPDF-Converter/pdfGenerator"
+	"fmt"
+)
+
+func main() {
+
+	r := u.NewRequestPdf("")
+
+	//html template path
+	templatePath := "templates/sample.html"
+
+	//path for download pdf
+	outputPath := "storage/example.pdf"
+
+	//html template data
+	templateData := struct {
+		Title       string
+		Description string
+		Company     string
+		Contact     string
+		Country     string
+	}{
+		Title:       "HTML to PDF generator",
+		Description: "This is the simple HTML to PDF file.",
+		Company:     "Jhon Lewis",
+		Contact:     "Maria Anders",
+		Country:     "Germany",
+	}
+
+	if err := r.ParseTemplate(templatePath, templateData); err == nil {
+		ok, _ := r.GeneratePDF(outputPath)
+		fmt.Println(ok, "pdf generated successfully")
+	} else {
+		fmt.Println(err)
+	}
+}
---
+++ pdfGenerator/pdf.go
@@ -0,0 +1,76 @@
+package pdfGenerator
+
+import (
+	"bytes"
+	"github.com/SebastiaanKlippert/go-wkhtmltopdf"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+type RequestPdf struct {
+	body string
+}
+
+func NewRequestPdf(body string) *RequestPdf {
+	return &RequestPdf{
+		body: body,
+	}
+}
+
+func (r *RequestPdf) ParseTemplate(templateFileName string, data interface{}) error {
+
+	t, err := template.ParseFiles(templateFileName)
+	if err != nil {
+		return err
+	}
+	buf := new(bytes.Buffer)
+	if err = t.Execute(buf, data); err != nil {
+		return err
+	}
+	r.body = buf.String()
+	return nil
+}
+
+func (r *RequestPdf) GeneratePDF(pdfPath string) (bool, error) {
+	t := time.Now().Unix()
+	// write whole the body
+	err1 := ioutil.WriteFile("cloneTemplate/"+strconv.FormatInt(int64(t), 10)+".html", []byte(r.body), 0644)
+	if err1 != nil {
+		panic(err1)
+	}
+
+	f, err := os.Open("cloneTemplate/" + strconv.FormatInt(int64(t), 10) + ".html")
+	if f != nil {
+		defer f.Close()
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pdfg, err := wkhtmltopdf.NewPDFGenerator()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pdfg.AddPage(wkhtmltopdf.NewPageReader(f))
+
+	pdfg.PageSize.Set(wkhtmltopdf.PageSizeA4)
+
+	pdfg.Dpi.Set(300)
+
+	err = pdfg.Create()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = pdfg.WriteFile(pdfPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return true, nil
+}
